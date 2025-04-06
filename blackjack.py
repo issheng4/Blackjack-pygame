a04959093b5e8999d77d2486bf1e4aa93a95f8c6
issheng4 <115046931+issheng4@users.noreply.github.com>
2025-04-06 22:13:01 +0100

    refactor: modularise logic into functions

--- blackjack.py
+++ blackjack.py
@@ -10,36 +10,99 @@ for i in VALUES:
         deck.append((i, j))
 
 
-# initialise hands and scores
+# initialise player variables
+player_name = 'player'
 player_hand = []
-dealer_hand = []
 player_total = 0
+player_ace_eleven_count = 0
+
+# initialise dealer variables
+dealer_name = 'dealer'
+dealer_hand = []
 dealer_total = 0
+dealer_ace_eleven_count = 0
 
 
-# function to convert value on card to a score
-def value_to_score(value):
-    if value in ['J', 'Q', 'K']:
-        return 10
-    elif value == 'A':
-        return 11
-    else:
-        return int(value)
+
+
+# helper functions:
+
+def receive_card(deck, hand, total, ace_as_eleven_count):
+    '''person receives card, including adding onto points and ace check'''
+ 
+    def value_to_points(value):
+        '''convert value on card to points'''
+        if value in ['J', 'Q', 'K']:
+            return 10
+        elif value == 'A':
+            return 11
+        else:
+            return int(value)
+        
+    def update_ace_status(card, ace_as_eleven_count, total):
+        '''determine whether an Ace should score 1 or 11'''
+        if card == 'A':
+            ace_as_eleven_count += 1
+            if total > 21 and ace_as_eleven_count:
+                total -= 10
+                ace_as_eleven_count -= 1
+        return ace_as_eleven_count, total
     
-# function to display player's hand onscreen
+    hand.append(deck.pop())
+    card = hand[-1][0]
+    total += value_to_points(card)
+    ace_as_eleven_count, total = update_ace_status(card, ace_as_eleven_count, total)
+    return deck, hand, total, ace_as_eleven_count
+
+ 
+def dialogue_next_line():
+    '''player presses enter key to continue'''
+    print()
+    input('(press enter to continue.....)')
+    print()
+
+
+def perform_endgame(winner, winner_points, loser=0, loser_points=0):
+    print()    
+    if winner == 'none':
+        print('*** GAME DRAW ***')
+        print(f"both the player and dealer have {winner_points}!")
+        print()
+        print()
+        quit()
+    
+    print(f'*** {winner.upper()} WINS ***')
+
+    if winner_points == 'blackjack':
+        print(f'{winner} got a blackjack!')
+        print()
+        print()
+        quit()
+    if loser_points > 21:
+        print(f'{loser} went bust!')
+        print()
+        print()
+        quit()
+    print(f'{winner} had {winner_points} // {loser} had {loser_points}')
+    print()
+    print()
+    quit()
+
+
+
+""" # may implement this back in
 def show_player_hand():
+    '''display player's hand onscreen'''
     print()
     print(f"////  player's hand: {str(player_hand)}  TOTAL: {str(player_total)}  ////")
     print ()
 
-# function to display dealer's hand onscreen
+
 def show_dealer_hand():
+    '''display dealer's hand onscreen'''
     print(f"////  dealer's hand: {str(dealer_hand)}  TOTAL: {str(dealer_total)}  ////")
     print()
-
-# initialise Ace as 11 count
-player_ace_as_eleven_count = 0
-dealer_ace_as_eleven_count = 0
+"""
 
 
 
@@ -47,76 +110,40 @@ dealer_ace_as_eleven_count = 0
 
 # PHASE 1: dealing
 
+print()
+print()
+print("Welcome to this game of blackjack! Let's deal....")
+dialogue_next_line()
+
 
 # shuffle deck
 random.shuffle(deck)
 
-# player gets 1 card (visible)
-player_hand.append(deck.pop())
-player_total += value_to_score(player_hand[-1][0])
-### function: player ace check
-if player_hand[-1][0] == 'A':
-    player_ace_as_eleven_count += 1
-    if player_total > 21 and player_ace_as_eleven_count:
-        player_total -= 10
-        player_ace_as_eleven_count -= 1
-
-# dealer gets 1 card (visible)
-dealer_hand.append(deck.pop())
-dealer_total += value_to_score(dealer_hand[-1][0])
-### function: dealer ace check
-if dealer_hand[-1][0] == 'A':
-    dealer_ace_as_eleven_count += 1
-    if dealer_total > 21 and dealer_ace_as_eleven_count:
-        dealer_total -= 10
-        dealer_ace_as_eleven_count -= 1
-
-# player gets 1 more card (visible)
-player_hand.append(deck.pop())
-player_total += value_to_score(player_hand[-1][0])
-### function: player ace check
-if player_hand[-1][0] == 'A':
-    player_ace_as_eleven_count += 1
-    if player_total > 21 and player_ace_as_eleven_count:
-        player_total -= 10
-        player_ace_as_eleven_count -= 1
-
-# dealer gets 1 more card (hidden)
-dealer_hand.append(deck.pop())
-dealer_total += value_to_score(dealer_hand[-1][0])
-### function: dealer ace check
-if dealer_hand[-1][0] == 'A':
-    dealer_ace_as_eleven_count += 1
-    if dealer_total > 21 and dealer_ace_as_eleven_count:
-        dealer_total -= 10
-        dealer_ace_as_eleven_count -= 1
-
-
-
-# show cards
-show_player_hand()
-# show dealer hand including hidden card
-print(f"////  dealer's hand: {str(dealer_hand[0])}, [hidden card]  ////")
+deck, player_hand, player_total, player_ace_eleven_count = receive_card(deck, player_hand, player_total, player_ace_eleven_count)
 print()
+print(f"//// {player_name}'s hand: {str(player_hand)}  ////")
+print ()
+deck, dealer_hand, dealer_total, dealer_ace_eleven_count = receive_card(deck, dealer_hand, dealer_total, dealer_ace_eleven_count)
+print()
+print(f"//// {dealer_name}'s hand: {str(dealer_hand)}  ////")
+print ()
 
+dialogue_next_line()
+
+deck, player_hand, player_total, player_ace_eleven_count = receive_card(deck, player_hand, player_total, player_ace_eleven_count)
+print()
+print(f"//// {player_name}'s hand: {str(player_hand)}  TOTAL: {str(player_total)}  ////")
+print ()
+deck, dealer_hand, dealer_total, dealer_ace_eleven_count = receive_card(deck, dealer_hand, dealer_total, dealer_ace_eleven_count)
+print()
+print(f"//// {dealer_name}'s hand: [{str(dealer_hand[0])}, (hidden card)]  ////")
+print ()
 
 # blackjack checks
 if player_total == 21:
-    if value_to_score(dealer_hand[0][0]) >= 10:
-        print("Player has a blackjack?? Let's see if the dealer does too")
-        print()
-        show_dealer_hand()
-        # if dealer has a blackjack
-        if dealer_total == 21:
-            print("***GAME DRAW: both the player and dealer have blackjacks!***")
-            print()
-            quit()
-    print("***PLAYER WINS: blackjack!***")
-    print()
-    quit()
-
-
-
+    if dealer_total == 21:
+        perform_endgame('none', 'blackjack')
+    perform_endgame(player_name, 'blackjack', dealer_name)
 
 
 
@@ -129,28 +156,19 @@ move = 0
 
 # while player chooses a move other than stand
 while move != 's':
-    print("Hit or stick?")
-    # player chooses move
-    move = input("(type 'h' to hit and 's' to stick) ")
     print()
-    # if player chooses to hit
+    print('Hit or stick?')
+    print("(type 'h' to hit or 's' to stick)")
+    move = input()
+    print()
     if move == 'h':
-        # player receives another card
-        player_hand.append(deck.pop())
-        player_total += value_to_score(player_hand[-1][0])
-        ### function: player ace check
-        if player_hand[-1][0] == 'A':
-            player_ace_as_eleven_count += 1
-            if player_total > 21 and player_ace_as_eleven_count:
-                player_total -= 10
-                player_ace_as_eleven_count -= 1
-        show_player_hand()
-
-        # check if player is bust
+        deck, player_hand, player_total, player_ace_eleven_count = receive_card(deck, player_hand, player_total, player_ace_eleven_count)
+        print()
+        print(f"//// {player_name}'s hand: {str(player_hand)}  TOTAL: {str(player_total)}  ////")
+        print ()
+        #check if player is bust
         if player_total > 21:
-            print("***DEALER WINS: player has bust!***")
-            print()
-            quit()
+            perform_endgame(dealer_name, dealer_total, player_name, player_total)
 
 
 print('Player chose to stand')
@@ -158,62 +176,51 @@ print()
 
 
 
-
-
-
 # PHASE 3: dealer's turn
 
 # dealer reveals card
-print("The dealer shall reveal their hidden card")
+print("Dealer's turn.... now to reveal their hidden card....")
+dialogue_next_line()
 print()
-show_dealer_hand()
+print(f"//// {dealer_name}'s hand: {str(dealer_hand)}  TOTAL: {str(dealer_total)}  ////")
+print ()
+dialogue_next_line()
 
 # blackjack check
 if dealer_total == 21:
-    print("***DEALER WINS: blackjack!***")
-    print()
-    quit()
+    perform_endgame(dealer_name, 'blackjack', player_name)
 
 
-# dealer hits until score is at least 17
-while dealer_total < 17:
-    # dealer receives another card
-    dealer_hand.append(deck.pop())
-    dealer_total += value_to_score(dealer_hand[-1][0])
-    ### function: dealer ace check
-    if dealer_hand[-1][0] == 'A':
-        dealer_ace_as_eleven_count += 1
-        if dealer_total > 21 and dealer_ace_as_eleven_count:
-            dealer_total -= 10
-            dealer_ace_as_eleven_count -= 1
-    show_dealer_hand()
-
-        # check if dealer is bust
-if dealer_total > 21:
-    print("***PLAYER WINS: dealer has bust!***")
-    print()
-    quit()
-
-
-
-
-
-
-# PHASE 4: reviewing hands
-print("Time to review the hands")
-
-if player_total > dealer_total:
-    print(f"***PLAYER WINS: player scored {player_total} and dealer scored {dealer_total}***")
-    print()
-    quit()
-elif player_total < dealer_total:
-    print(f"***DEALER WINS: player scored {player_total} and dealer scored {dealer_total}***")
-    print()
-    quit()
+# dealer hits until points total at least 17
+if dealer_total >= 17:
+    print("Dealer must stand when they have at least 17 points.")
 else:
-    print(f"***GAME DRAW: both player and dealer scored {player_total}***")
+    print("Dealer must draw until they total at least 17 points.")
+dialogue_next_line()
+
+while dealer_total < 17:
+    deck, dealer_hand, dealer_total, dealer_ace_eleven_count = receive_card(deck, dealer_hand, dealer_total, dealer_ace_eleven_count)
     print()
-    quit()
+    print(f"//// {dealer_name}'s hand: {str(dealer_hand)}  TOTAL: {str(dealer_total)}  ////")
+    print ()
+    # check if dealer is bust
+    if dealer_total > 21:
+        perform_endgame(player_name, player_total, dealer_name, dealer_total)
+    dialogue_next_line()
+
+
+
+
+
+# PHASE 4: reviewing hands and revealling the winner
+print("Time to review the hands")
+dialogue_next_line()
+if player_total == dealer_total:
+    perform_endgame('none', player_total)
+elif player_total > dealer_total:
+    perform_endgame(player_name, player_total, dealer_name, dealer_total) 
+else:
+    perform_endgame(dealer_name, dealer_total, player_name, player_total)
 
 
 
